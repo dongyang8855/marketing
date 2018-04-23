5248fc984d55f516d1387a5bf2747414f3821521
994619413@qq.com <dy930514>
2018-04-23 16:10:52 +0800

    .

---
+++ build/webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const vueLoaderConfig = require('./vue-loader.conf');
+
+function resolve(dir) {
+	return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+	entry: path.resolve(__dirname, '../src/main.js'),
+	output: {
+		path: path.resolve(__dirname, '../server/public'),
+		filename: 'index.js'
+	},
+	resolve: {
+		extensions: ['.js', '.vue', '.json'],
+		alias: {
+			'vue$': 'vue/dist/vue.esm.js',
+			'@': resolve('src'),
+		}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader',
+				options: vueLoaderConfig
+			},
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				include: [resolve('src'), resolve('node_modules/webpack-dev-server/client')]
+			},
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader'
+				]
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+				loader: 'url-loader'
+			},
+			{
+				test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+				loader: 'url-loader'
+			},
+			{
+				test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+				loader: 'url-loader'
+			}
+		]
+	},
+	devtool: "source-map"
+};
